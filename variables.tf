e517d4999b79d3aeda0556842803d9f295d37b6b
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-17 16:12:54 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+variable "hcp_bucket_ubuntu" {
+  description = "The Bucket where our AMI is listed."
+  default     = "labyrinth"
+}
+
+variable "hcp_channel" {
+  description = "HCP Packer channel name"
+  default     = "labyrinth-channel"
+}
+
+variable "tfc_org_name" {
+  description = "Name of the Terraform Cloud Organization"
+  type        = string
+  default     = "se-intern-project"
+}
+
+variable "tfc_network_workspace_name" {
+  description = "Name of the network workspace"
+  type        = string
+  default     = "labyrinth-network"
+}
