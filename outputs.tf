704202ced6a843c2c8fc7348df58768aa9414cd0
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-23 09:29:20 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,32 @@ output "public_dns_name" {
   value = data.terraform_remote_state.network.outputs.public_dns_name
 }
 */
+
 #outputs app url
 output "app_url" {
   value = "http://${module.ec2_instance.public_ip}"
 }
+
+output "db_instance_endpoint" { 
+value = data.aws_db_instance.labyrinth-db.endpoint 
+} 
+
+output "db_instance_port" { 
+value = data.aws_db_instance.labyrinth-db.port 
+} 
+
+output "db_instance_name" { 
+value = data.aws_db_instance.labyrinth-db.db_name 
+} 
+
+output "db_instance_username" { 
+value = data.aws_db_instance.labyrinth-db.username 
+}
+
+
+
+
+
+
+
+
