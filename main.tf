6af5a14d0946a2dc5eb9ed037cfa751eaab436db
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-25 16:02:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,22 +68,3 @@ resource "aws_key_pair" "labyrinth_kp" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.labyrinth.public_key_openssh
 }
-
-resource "aws_db_instance" "labyrinth-db" {
-  allocated_storage                     = 20
-  db_subnet_group_name                  = data.terraform_remote_state.network.outputs.subnet_group_name
-  engine                                = "mysql"
-  engine_version                        = "8.0.35"
-  identifier                            = "labyrinth-db"
-  instance_class                        = "db.m6gd.large"
-  parameter_group_name                  = "default.mysql8.0"
-  password                              = "HashicorpInterns2024!"
-  publicly_accessible                   = false
-  skip_final_snapshot                   = true
-  username                              = "admin"
-  vpc_security_group_ids                = data.terraform_remote_state.network.outputs.rds_security_group_id
-
-  tags = {
-    Name = "Labyrinth Database"
-  }
-}
