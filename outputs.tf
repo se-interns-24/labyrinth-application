d3f71a55106bbfbf0c5a6c40570576adf97b6a58
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-25 15:44:36 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,23 +9,6 @@ output "app_url" {
   value = "http://${module.ec2_instance.public_ip}"
 }
 
-output "db_instance_endpoint" { 
-value = aws_db_instance.labyrinth-db.endpoint 
-} 
-
-output "db_instance_port" { 
-value = aws_db_instance.labyrinth-db.port 
-} 
-
-output "db_instance_name" { 
-value = aws_db_instance.labyrinth-db.db_name 
-} 
-
-output "db_instance_username" { 
-value = aws_db_instance.labyrinth-db.username 
-}
-
-
 
 
 
