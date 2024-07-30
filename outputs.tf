23530cf9e0639f36f061eb99abf00523f3165cb9
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-30 11:12:52 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
-/*
-output "public_dns_name" {
-  value = data.terraform_remote_state.network.outputs.public_dns_name
-}
-*/
-
 #outputs app url
 output "app_url" {
   value = "http://${module.ec2_instance.public_ip}"
 }
-/*
-output "instance-id" {
-  value = aws_instance.this.id
-}*/
