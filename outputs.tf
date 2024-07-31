448b73f480cd75e49e61f1ab496f73d39cf74c2e
ayushisapru <89663804+ayushisapru@users.noreply.github.com>
2024-07-31 10:33:28 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,18 +3,3 @@ output "app_url" {
   value = "http://${module.ec2_instance.public_ip}"
 }
 
-output "nlb_arn" {
-  value = aws_lb.nlb.arn
-}
-
-output "nlb_dns_name" {
-  value = aws_lb.nlb.dns_name
-}
-
-output "alb_arn" {
-  value = module.alb.alb_arn
-}
-
-output "alb_dns_name" {
-  value = module.alb.alb_dns_name
-}
