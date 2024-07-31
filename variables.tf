99827a33b7f104c65e8efe81f833111b06132023
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-07-31 09:28:44 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,9 +17,9 @@ variable "tfc_org_name" {
   type        = string
   default     = "se-intern-project"
 }
-/*
+
 variable "tfc_network_workspace_name" {
   description = "Name of the network workspace"
   type        = string
-  default     = "labyrinth-network-dev"
-}*/
+  default     = "labyrinth-network-stage"
+}
