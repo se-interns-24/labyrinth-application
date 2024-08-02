1c6e626f2c1f867c5e91c2ef69c9dce8ee4a8490
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-02 15:36:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 #defining providers
-
 provider "hcp" {
 }
-
 
 data "terraform_remote_state" "network" {
   backend = "remote"
@@ -17,7 +15,6 @@ data "terraform_remote_state" "network" {
 
 provider "aws" {
   region = data.terraform_remote_state.network.outputs.region
-  //region = var.region
 }
 
 #defining ec2 instance using module - FE - all outputs from networking resources
@@ -25,7 +22,7 @@ module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "my-ec2-instance"
+  name = var.instance_name
   instance_type = var.instance_type
   key_name = aws_key_pair.labyrinth_kp.key_name
   ami = data.hcp_packer_image.ubuntu.cloud_image_id
@@ -48,17 +45,17 @@ data "hcp_packer_iteration" "ubuntu" {
 data "hcp_packer_image" "ubuntu" {
   bucket_name    = data.hcp_packer_iteration.ubuntu.bucket_name
   iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
-  cloud_provider = "aws"
+  cloud_provider = var.cloud_provider
   region         = data.terraform_remote_state.network.outputs.region
 }
 
 #generates private key to use for key pair - FE
 resource "tls_private_key" "labyrinth" {
-  algorithm = "RSA"
+  algorithm = var.algorithm
 }
 #defines local var for key file name - FE
 locals {
-  private_key_filename = "labyrinth-key.pem"
+  private_key_filename = var.private_key_filename
 }
 
 #creates key pair used to access ec2 instance - FE
@@ -69,7 +66,7 @@ resource "aws_key_pair" "labyrinth_kp" {
 
 # Defines the Elastic IP
 resource "aws_eip" "labyrinth-eip" {
-  domain = "vpc"
+  domain = var.domain
 
   tags = {
     Name = "MyEC2Instances"
@@ -80,4 +77,31 @@ resource "aws_eip" "labyrinth-eip" {
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = module.ec2_instance.id
   allocation_id = aws_eip.labyrinth-eip.id
+}
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.9.0"
+
+  name                       = var.alb_name
+  vpc_id                     = data.terraform_remote_state.network.outputs.vpc_id
+  internal                   = var.alb_internal
+  security_groups            = data.terraform_remote_state.network.outputs.security_group_id
+  subnets                    = [data.terraform_remote_state.network.outputs.subnet_id[0], data.terraform_remote_state.network.outputs.subnet_id_b[0]]
+  enable_deletion_protection = var.enable_deletion_protection
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.7.0"
+
+  name                  = var.asg_name
+  min_size              = var.min_size
+  max_size              = var.max_size
+  desired_capacity      = var.desired_capacity
+  health_check_type     = var.health_check_type
+  vpc_zone_identifier   = [data.terraform_remote_state.network.outputs.subnet_id[0]]
+  instance_type         = var.instance_type
+  image_id              = data.hcp_packer_image.ubuntu.cloud_image_id
 }
