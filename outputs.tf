2f411e14648c13786909a9268e2dba5ffce7ecb7
sfgtrrz <sofia.gutierrez@hashicorp.com>
2024-08-05 11:08:46 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-/*
-output "public_dns_name" {
-  value = data.terraform_remote_state.network.outputs.public_dns_name
-}
-*/
 #outputs app url
 output "app_url" {
   value = "http://${module.ec2_instance.public_ip}"
